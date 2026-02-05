5e53f4e0d9523205879a2472b1ad0e70318b3817
enter-pro <github@enter.pro>
2026-02-05 05:27:38 +0000

    refactor(router): refactor app routing with useRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "@/router";
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import { routers } from "@/router";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+
+function AppRoutes() {
+  const routes = useRoutes(routers);
+  return routes;
+}
 
 function App() {
   return (
     <div className="dark">
       <BrowserRouter>
-        <AppRouter />
+        <AppRoutes />
         <Toaster />
         <Sonner />
       </BrowserRouter>
